969b5390431d699ecdd934e5aa4bb850e5b4975f
Maxwell Mullin <mullinmax@gmail.com>
2023-12-15 18:51:45 -0500

    adding basic docker parsing

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import docker
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import os
 
@@ -11,6 +12,24 @@ def run(server_class=HTTPServer, handler_class=ServerHandler, port=5000):
     httpd = server_class(server_address, handler_class)
     print(f'Starting httpd server on port {port}')
     httpd.serve_forever()
+
+def get_flip_flop_urls():
+    client = docker.from_env()
+    containers = client.containers.list()
+    urls = []
+
+    for container in containers:
+        labels = container.labels
+        flip_flop_url = labels.get('flip-flop.url')
+        if flip_flop_url:
+            urls.append(flip_flop_url)
+
+    return urls
+
+# Example usage
+urls = get_flip_flop_urls()
+print(urls)
+
 
 if __name__ == '__main__':
     run()
