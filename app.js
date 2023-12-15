427c8f08cb5101cb2ed79011a46a12c3d3ef32ae
Maxwell Mullin <mullinmax@gmail.com>
2023-12-14 21:29:10 -0500

    right side menu

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-// Sample data, replace with dynamic data from config file later
 const apps = [
-    { name: "Plex", url: "https://plex.doze.dev"},
+    { name: "Plex", url: "https://plex.doze.dev" },
     { name: "Flame", url: "https://start.doze.dev", icon: "🔥" },
     { name: "Media Status Dashboard", url: "https://uptime.doze.dev/status/plex" },
-    { name: "Media Requests", url: "https://requests.doze.dev"}
+    { name: "Media Requests", url: "https://requests.doze.dev" }
 ];
 
 const tabContainer = document.getElementById('tab-container');
 const appFrame = document.getElementById('app-frame');
 
 apps.forEach(app => {
-    function getDomain(url) {
-        const a = document.createElement('a');
-        a.href = url;
-        return a.hostname;
-    }
-
-    function getFaviconImg(url) {
-        if (url.startsWith('http')) {
-            return `<img src="${url}" alt="" style="width: 16px; height: 16px; vertical-align: middle; margin-right: 8px;">`;
-        } else {
-            return `<span style="font-size: 16px; vertical-align: middle; margin-right: 8px;">${url}</span>`;
-        }
-    }
-
     const faviconImg = app.icon ? getFaviconImg(app.icon) : `<img src="http://${getDomain(app.url)}/favicon.ico" alt="" style="width: 16px; height: 16px; vertical-align: middle; margin-right: 8px;">`;
 
     const tab = document.createElement('button');
@@ -36,27 +21,48 @@ apps.forEach(app => {
     };
     tabContainer.appendChild(tab);
 });
-// Find the first button in the tabContainer and click it
+
 const firstButton = tabContainer.querySelector('button');
 if (firstButton) {
     firstButton.click();
 }
 
-
-let hoverTimeout;
-
-tabContainer.onmouseover = () => {
-    clearTimeout(hoverTimeout);
-    tabContainer.style.top = '0';
-};
-
-tabContainer.onmouseout = () => {
-    hoverTimeout = setTimeout(() => {
-        tabContainer.style.top = '-50px'; // Adjust based on your tab container height
-    }, 3000); // Time in milliseconds; adjust as needed
-};
-
-function toggleTabBar() {
-    const currentTop = tabContainer.style.top;
-    tabContainer.style.top = currentTop === '0px' ? '-50px' : '0';
+function getDomain(url) {
+    const a = document.createElement('a');
+    a.href = url;
+    return a.hostname;
 }
+
+function getFaviconImg(url) {
+    if (url.startsWith('http')) {
+        return `<img src="${url}" alt="" style="width: 16px; height: 16px; vertical-align: middle; margin-right: 8px;">`;
+    } else {
+        return `<span style="font-size: 16px; vertical-align: middle; margin-right: 8px;">${url}</span>`;
+    }
+}
+
+let inactivityTimer;
+
+function resetTimer() {
+    clearTimeout(inactivityTimer);
+    inactivityTimer = setTimeout(() => {
+        if (window.innerWidth < 768 && tabContainer.style.right === '0px') {
+            toggleMenu();
+        }
+    }, 5000); // 5 seconds of inactivity
+}
+
+function toggleMenu() {
+    tabContainer.style.right = tabContainer.style.right === '0px' ? '-200px' : '0px';
+}
+
+window.onload = resetTimer;
+document.addEventListener('touchstart', resetTimer);
+document.addEventListener('click', resetTimer);
+document.addEventListener('scroll', resetTimer);
+
+window.onload = () => {
+    if (window.innerWidth < 768) {
+        tabContainer.style.right = '-200px';
+    }
+};
